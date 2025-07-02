45dced8aa16dbe1a88b9a86eb0a5667476a99bf6
Andrew Heberle <andrew.heberle@r-group.com.au>
2025-07-02 12:34:58 +0800

    Add test/example

--- pkg/sshagent/sshagent.go
+++ pkg/sshagent/sshagent.go
@@ -4,10 +4,18 @@ import (
 	"golang.org/x/crypto/ssh/agent"
 )
 
+// Agent wraps agent.ExtendedAgent
 type Agent struct {
 	agent.ExtendedAgent
 }
 
+// NewAgent connects to a local SSH agent either via unix socket or named pipes depending on the OS
+//
+// In the case of a non-Windows OS, the "SSH_AUTH_SOCK" environment variable must be set or the
+// process will fail.
+//
+// On Windows a connection is attempted to "\\.\pipe\openssh-ssh-agent" which is the default
+// named pipe path for the native OpenSSH Authentication Agent.
 func NewAgent() (*Agent, error) {
 	conn, err := connect()
 	if err != nil {
